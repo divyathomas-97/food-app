baa8a3b846f667dfe2aa003c6643f8c2f55141c2
divyathomas-97 <81955847+divyathomas-97@users.noreply.github.com>
2026-04-07 16:32:27 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,3 @@
-// import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// export function HttpLoaderFactory() {
-//   return new TranslateHttpLoader();
-// }
-
-// @Component({
-//   selector: 'app-root',
-//   imports: [RouterOutlet, TranslateModule],
-//   templateUrl: './app.html',
-//   styleUrl: './app.css',
-//   providers: [
-//     {
-//       provide: TranslateLoader,
-//       useFactory: HttpLoaderFactory,
-//       deps: []
-//     }
-//   ]
-// })
-// export class App {
-//   protected readonly title = signal('food-app');
-// }
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -30,6 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>` // ✅ REQUIRED
+  template: `<router-outlet></router-outlet>`
 })
 export class App {}
